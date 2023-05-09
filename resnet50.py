bd81efaa9695e0fe959add2a24b6982b3b57d78a
Vinayak Mane <103372852+Vinayakmane47@users.noreply.github.com>
2023-05-09 11:25:18 +0530

    Create resnet50.py

---
+++ resnet50.py
@@ -0,0 +1,73 @@
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Model
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+
+
+# Load the pre-trained ResNet50 model
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+
+# Freeze the layers in the base model
+for layer in base_model.layers:
+    layer.trainable = False
+    
+    
+# Define the parameters for data augmentation
+data_dir = "/kaggle/input/dogs-breed-dataset/dog_v1"
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2,
+    rescale=1./255
+)
+
+# Define the training and validation generators
+train_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Add new classification layers on top of the base model
+x = base_model.output
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.3)(x)
+x = Dense(5, activation='softmax')(x)
+
+# Define the new model
+model = Model(inputs=base_model.input, outputs=x)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+## Define Early Stopping : 
+early_stopping = EarlyStopping(patience=10, monitor='val_loss', restore_best_weights=True)
+
+# Train the model
+history = model.fit_generator(
+    train_generator,
+    epochs=10,
+    validation_data=validation_generator,
+    callbacks=[early_stopping],
+    steps_per_epoch=train_generator.n//train_generator.batch_size,
+    validation_steps=validation_generator.n//validation_generator.batch_size
+)
