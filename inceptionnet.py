4f428af775154fe19987948869e05f63d2fa1a37
Vinayak Mane <103372852+Vinayakmane47@users.noreply.github.com>
2023-05-09 11:28:59 +0530

    Create inceptionnet.py

---
+++ inceptionnet.py
@@ -0,0 +1,66 @@
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping
+
+# Load the pre-trained InceptionNet model
+base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+
+# Add a global average pooling layer and a dense layer for classification
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.3)(x)
+predictions = Dense(5, activation='softmax')(x)
+
+# Define the complete model
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Freeze the pre-trained layers in the base model
+for layer in base_model.layers:
+    layer.trainable = False
+data_dir = "/kaggle/input/dogs-breed-dataset/dog_v1"
+# Define the parameters for data augmentation
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2,
+    rescale=1./255
+)
+
+# Define the training and validation generators
+train_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+## Define Early Stopping : 
+early_stopping = EarlyStopping(patience=10, monitor='val_loss', restore_best_weights=True)
+
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(train_generator,
+                    epochs=10,
+                    validation_data=validation_generator,
+                    callbacks=[early_stopping],
+                    verbose=1)
